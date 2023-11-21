3a5028a9affcb3991d5664a05556880b8f2ea37e
Joel Martinez <joelmartinez@gmail.com>
2023-11-20 20:22:49 -0500

    lvgl is now compiling, right now it's compiling the lvgl_demos target, so need to get it compiling the main library target

--- generator/bindings.ps1
+++ generator/bindings.ps1
@@ -24,6 +24,15 @@ cp ./LVGLSharp.csproj ./tmp/LVGLSharp/LVGLSharp.csproj
 
 # compile lvgl
 push-location ./tmp/lvgl
+
+# https://docs.lvgl.io/8.3/get-started/platforms/arduino.html#configure-lvgl
+$confPath = "./src/lv_conf.h"
+
+cp ./lv_conf_template.h $confPath
+$r = (Get-Content -Path $confPath) -replace '#if 0', '#if 1'
+$r | Set-Content -Path $confPath
+
+# now run cmake
 mkdir build
 cd build
 cmake -DBUILD_SHARED_LIBS=TRUE ..
